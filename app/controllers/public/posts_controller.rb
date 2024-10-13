90c7b6905321051a22ed69ca4f0fe274851cc966
SK6595 <173334721+SK6595@users.noreply.github.com>
2024-10-13 13:57:27 +0000

    新規投稿フォームの作成途中

---
+++ app/controllers/public/posts_controller.rb
@@ -0,0 +1,28 @@
+class Public::PostsController < ApplicationController
+
+  def new
+  end
+  
+  def number
+    
+  end
+
+  def index
+  end
+
+  def show
+    @friends = Friend.all
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
 
-  root to: "homes#top"
+  namespace :admin do
+    get 'homes/top'
+  end
+  
+  devise_for :users, controllers: {
+    sessions: 'public/users/sessions',
+    registrations: 'public/users/registrations'
+  }
+  
+  devise_for :admins, controllers: {
+    sessions: 'admin/admins/sessions'
+  }
+  
+  scope module: :public do
+    root "homes#top"
+    resources :posts do
+      resources :comments
+      resources :favorites
+    end
+  end
+
+  namespace :admin do
+    root "homes#top"
+  end
 end
