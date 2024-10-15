6628b19d69aeff42d153bdb03da3444a336872c1
SK6595 <173334721+SK6595@users.noreply.github.com>
2024-10-15 08:08:03 +0000

    画像投稿機能作成途中

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,20 +1,24 @@
 class Public::PostsController < ApplicationController
 
   def new
-  end
-  
-  def number
-    
+    @post = Post.new
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
-    @friends = Friend.all
   end
 
   def create
+    #データを受け取り新規登録するためのインスタンス作成
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    @post.save
+    # 4. トップ画面へリダイレクト
+    redirect_to posts_path
   end
 
   def edit
@@ -26,3 +30,9 @@ class Public::PostsController < ApplicationController
   def destroy
   end
 end
+
+private
+  # ストロングパラメータ
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
