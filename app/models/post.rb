82fbf9fe507016e14f1660ee58ba00e04d734119
SK6595 <173334721+SK6595@users.noreply.github.com>
2024-12-14 15:44:31 +0000

    実装フェーズ3完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,14 +6,10 @@ class Post < ApplicationRecord
   validates :address, presence: true
 
   belongs_to :user
-  #belonngs_to :userと同義
-  #def user
-  #  User.find(self.user_id)
-  #end
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :notifications, as: :notifiable, dependent: :destroy
-  
+
   geocoded_by :address
   after_validation :geocode
 
@@ -22,7 +18,6 @@ class Post < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       self.image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    #byebug
     self.image
   end
 
@@ -33,7 +28,7 @@ class Post < ApplicationRecord
   def self.search_for(content)
     Post.where('title LIKE ?', '%'+content+'%')
   end
-  
+
   after_create do
     user.followers.each do |user|
     notifications.create(user_id: user.id)
